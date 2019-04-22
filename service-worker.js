3e715c6ef0689356cfd20cc45b8ea2d8e094016d
James D <hello@jamesin.space>
2019-04-22 22:54:00 +0100

    Make PWA; improve accessibility

---
+++ service-worker.js
@@ -0,0 +1,28 @@
+/* global workbox */
+
+workbox.core.setCacheNameDetails({
+  prefix: 'mrvs',
+  suffix: 'v1',
+  precache: 'precache',
+  runtime: 'runcache'
+})
+
+workbox.skipWaiting()
+workbox.clientsClaim()
+
+workbox.precaching.precacheAndRoute(self.__precacheManifest)
+
+workbox.routing.registerRoute(
+  /\.html$/,
+  workbox.strategies.networkFirst()
+)
+
+workbox.routing.registerRoute(
+    /assets\/images/,
+    workbox.strategies.cacheFirst()
+)
+
+workbox.routing.registerRoute(
+    /^https?:\/\/cdn./,
+    workbox.strategies.staleWhileRevalidate()
+)
